ba7da21c7dae7cc59407158700209ae972fa661d
juangunilla <gunillajuan@gmail.com>
2022-09-01 19:16:15 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+var lista = new Array("img/arboles.jpg", "img/nieve.jpg");
+
+var contador = 0;
+
+var tempor = null;
+
+function cambio(sen) {
+  contador += sen;
+
+  if (contador == lista.length) contador = 0;
+  else if (contador < 0) contador = lista.length - 1;
+
+  document.images.centro.src = lista[contador];
+
+  window.status = "Imagen número " + contador;
+}
+
+function reset() {
+  contador = 0;
+
+  parar();
+
+  document.images.centro.src = lista[0];
+}
+
+function parar() {
+  if (tempor != null) clearTimeout(tempor);
+}
+
+function automat(tiempo, inc = 1) {
+  cambio(inc);
+
+  tempor = setTimeout(automat, tiempo, tiempo, inc);
+}
